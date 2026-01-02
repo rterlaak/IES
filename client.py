b9c6f9b5676460f4098129b0e20783927af71412
Reijer Ter Laak <rterlaak@tudelft.nl>
2026-01-02 15:01:59 +0100

    soort van een begin

--- client.py
+++ client.py
@@ -16,6 +16,13 @@ def recvall(sock, n):
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 clientSocket.connect((host, port))
 
+clientSocket.sendall(b"LIST")
+files_json = clientSocket.recv(1024).decode()
+files = json.loads(files_json)
+
+for i, name in enumerate(files, 1):
+    print(f"{i}. {name}")
+
 filename = input("Which file would you like to download: ")
 clientSocket.send(filename.encode())
 
@@ -30,6 +37,8 @@ with open("downloaded_" + filename, "wb") as f:
     remaining = size
     while remaining > 0:
         chunk = clientSocket.recv(4096 if remaining >= 4096 else remaining)
+        if not chunk:
+            raise ConnectionError
         f.write(chunk)
         remaining -= len(chunk)
 
