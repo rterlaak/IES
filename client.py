91cfc41a4f225bf671e61c1ce4e84817967ac37c
Tijmen van der Werf <twvanderwerf@tudelft.nl>
2026-01-05 15:40:59 +0100

    begin menu met goede versie

--- client.py
+++ client.py
@@ -13,3 +13,33 @@ receivedMsg = clientSocket.recv(1024).decode()
 print("Server response:")
 print(receivedMsg)
 clientSocket.close()
+
+def Menu():
+    while True:
+        print("Please make your choice:")
+        print("1  -  View files")
+        print("2  -  Download files")
+        print("3  -  Upload files")
+        print("4  -  Chat")
+        print("5  -  logout")
+
+        choice = input("User choice: ")
+        if choice == "1":
+            #code voor view
+        elif choice == "2":
+            #code voor dowload
+
+        elif choice == "3":
+            #vode voor Upload
+
+        elif choice == "4":
+            #code voor Chat
+
+        elif choice == "5":
+            print("User logout")
+            break
+        else:
+            print("Invalid choice")
+
+if __name__ == "__main__":
+    Menu()
