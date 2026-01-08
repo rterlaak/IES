e6b5832b9db3fed751140b146ff6f15020118b46
Tijmen van der Werf <twvanderwerf@tudelft.nl>
2026-01-08 12:39:22 +0100

    k

--- server.py
+++ server.py
@@ -1,7 +1,20 @@
+import os
 import socket
+import struct
 
 host = "localhost"
 port = 12000
+
+def send_file(connectedClient, path):
+    size = os.path.getsize(path)
+    connectedClient.sendall(struct.pack("!Q", size))
+    with open(path, "rb") as f:
+        while true:
+            chunk = f.read(4096)
+            if not chunk:
+                break
+            connectedClient.sendall(chunk)
+
 
 serverSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 serverSocket.bind((host, port))
@@ -11,14 +24,32 @@ serverSocket.listen(1)
 print("Socket is listening...")
 
 while True:
-     connectedClient, clientAddress = serverSocket.accept()
-     print(f"Connected to: {clientAddress[0]} : {clientAddress[1]}")
-     decodedString = (connectedClient.recv(1024).decode())
+    connectedClient, clientAddress = serverSocket.accept()
+    print(f"Connected to: {clientAddress[0]} : {clientAddress[1]}")
+    cmd = conn.recv(1024).decode().strip()
 
-     if decodedString[-4:] == ".txt":
-          to_send = "The received file is: "+ decodedString + "\n The files in the server directory are:"
-     else:
-          to_send = "INVALID FILE FORMAT"
+    if cmd != "LIST":
+        connectedClient.close()
+        continue
 
-     connectedClient.send(to_send.encode())
-     connectedClient.close()
+    base_dir = "sample_files"
+    files = [f for f in os.listdir(base_dir) if os.path.isfile(os.path.join(base_dir, f))]
+    connectedClient.sendall(json.dumps(files).encode())
+
+    requested = conn.recv(1024).decode().strip()
+    path = os.path.join(base_dir, requested)
+
+    if not os.path.isfile(filename):
+        connectedClient.sendall(struct.pack("!Q", 0))
+        connectedClient.close()
+        continue
+
+    size = os.path.getsize(filename)
+    print(f"File size: {size}")
+    connectedClient.sendall(struct.pack("!Q", size))
+
+
+
+
+
+    connectedClient.close()
