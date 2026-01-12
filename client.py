beb5382774b37b012b34b354fb6851e3a1d7f036
Reijer Ter Laak <rterlaak@tudelft.nl>
2026-01-12 09:38:45 +0100

    commit om te pullen

--- client.py
+++ client.py
@@ -135,6 +135,7 @@ def Menu():
                 print(f"File {filename} not found in {LOCAL_DIR}")
 
         elif choice == "4":
+            print("chat")
             #code voor Chat
 
         elif choice == "5":
